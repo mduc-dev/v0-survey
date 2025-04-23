7ec19fceba665de338e044c35d4360e86b49013f
mduc-dev <duc040301@gmail.com>
2025-04-23 11:51:19 +0700

    chore: rename to protected route in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define public routes that don't require authentication
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isProtectedRoutes = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isPublicRoute(req)) await auth.protect();
+  if (isProtectedRoutes(req)) await auth.protect();
 });
 
 export const config = {
